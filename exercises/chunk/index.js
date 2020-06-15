25438ad0a5fd74f0dd11a15fcb2c26fb25bd4256
RadenAR <radenabdulrahman@gmail.com>
2020-06-15 17:10:45 -0400

    solution 1 for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,11 +9,31 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk (array, size) {
-  const result = []
-  while (array.length > 0) {
-    result.push(array.splice(0, size))
-  }
-  return result
+  // solution done on my own
+  // const result = []
+  // while (array.length > 0) {
+  //   result.push(array.splice(0, size))
+  // }
+  // return result
+
+  // solution 1
+  // loop through array
+  // if nothing in result or if the last element is of size specified then make a new chunk
+  // else add element to the chunk that is already there
+  // const result = []
+  // for (let element of array) {
+  //   const last = result[result.length - 1]
+  //   if (!last || last.length === size) {
+  //     result.push([element])
+  //   } else {
+  //     last.push(element)
+  //   }
+  // }
+  // return result
+
+  // solution 2
+  // similar to own solution but using the slice method instead of splice
+  
 }
 
 module.exports = chunk
